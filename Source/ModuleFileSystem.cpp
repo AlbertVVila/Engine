4cefb8860bec83a052e8c93b3671d54cccbe10df
Albert Val Vila <albertvalvila1@gmail.com>
2018-12-04 19:51:03 +0100

    Added Physfs lib + Basic IO operations

--- Source/ModuleFileSystem.cpp
+++ Source/ModuleFileSystem.cpp
@@ -1,8 +1,5 @@
 #include "ModuleFileSystem.h"
 #include <assert.h>
-#include <iostream>
-#include <sys/stat.h>
-#include <direct.h>
 #include "SceneImporter.h"
 #include "physfs.h"
 
@@ -18,37 +15,18 @@ ModuleFileSystem::ModuleFileSystem()
 
 ModuleFileSystem::~ModuleFileSystem()
 {
+	PHYSFS_deinit(); //TODO: Add to list of libraries
+}
 
+bool ModuleFileSystem::Init()
+{
+	PHYSFS_init(NULL);
+	bool exists = Exists("nocamera.jpg");
+	return true;
 }
 
 unsigned int ModuleFileSystem::Load(const char * path, const char * file, char ** buffer) const
 {
-	//const char *extension = GetExtension(file);
-	//if (extension == "png" || extension == "jpg")
-	//{
-	//	//SceneImporter sceneImporter;
-	//	//std::string filepath(path);
-	//	//std::string filename(file);
-	//	//filepath += filename;
-	//	//sceneImporter.ImportMat(filepath.c_str());
-	//}
-	//else if (extension == "dds")
-	//{
-	//	//copy file if not in library/materials
-	//}
-	//else if (extension == "fbx" || extension == "FBX")
-	//{
-	//	//save to library/meshes as .dat
-	//	SceneImporter sceneImporter;
-	//	//std::string outputfile = "BakerHouse.dat";
-	//	//sceneImporter.ImportFBX(path, file, buffer);
-	//}
-	//else if (extension == "dat")
-	//{
-	//	//move to library/meshes 
-	//}
-	////SceneImporter sceneImporter;
-	////sceneImporter.ImportMat("Models\\Baker\\Baker_house.png");
 	return 0;
 }
 
@@ -62,74 +40,55 @@ bool ModuleFileSystem::Remove(const char * file) const
 	assert(file != nullptr);
 	if (file == nullptr) return false;
 
-	if (remove(file) != 0)
-	{
-		LOG("Error removing %s", file);
-		return false;
-	}
-	LOG("%s successfully removed", file);
-	return true;
 }
 
-bool ModuleFileSystem::Exists(const char * myfile) const
+bool ModuleFileSystem::Exists(const char * file) const
 {
-	if (FILE *file = fopen(myfile, "r")) {
-		fclose(file);
-		return true;
-	}
-	else {
-		return false;
-	}
+	assert(file != nullptr);
+	if (file == nullptr) return false;
+	return PHYSFS_exists(file);
+}
+
+unsigned ModuleFileSystem::Size(const char * file) const
+{
+	assert(file != nullptr);
+	if (file == nullptr) return 0;
+	PHYSFS_file* myfile = PHYSFS_openRead(file);
+	PHYSFS_sint64 file_size = PHYSFS_fileLength(myfile);
+	PHYSFS_close(myfile);
+	return file_size;
 }
 
 bool ModuleFileSystem::MakeDirectory(const char * directory)
 {
-#ifdef __linux__
-	if (mkdir(directory, 777) != 0)
+	assert(directory != nullptr);
+	if (directory == nullptr) return false;
+	int result = PHYSFS_mkdir(directory);
+	if (result != 0)
 	{
-		LOG("Eror on creating directory %s", directory);
-		return false
-	}
-	return true;
-#else
-	if (_mkdir(directory) != 0)
-	{
-		LOG("Eror on creating directory %s", directory);
+		LOG("Error: %s",PHYSFS_getLastError());
 		return false;
 	}
+
 	return true;
-#endif
 }
 
 bool ModuleFileSystem::IsDirectory(const char * file) const
 {
-	struct stat info;
-	if (stat(file, &info) != 0)
-	{
-		LOG("cannot access %s\n", file);
-		return false;
-	}
-	else if (info.st_mode & S_IFDIR)
-	{
-		LOG("%s is a directory\n", file);
-		return true;
-	}
-	else
-	{
-		LOG("%s is no directory\n", file);
-		return false;
-	}
+	assert(file != nullptr);
+	if (file == nullptr) return false;
+	return PHYSFS_isDirectory(file);
 }
 
-const char * ModuleFileSystem::GetExtension(const char *file) const
-{
-	std::string filename(file);
-	std::size_t found = filename.find_last_of(".");
-	if (std::string::npos != found)
-	{
-		filename.erase(0, found + 1);
-	}
-	return filename.c_str();
-}
+//const char * ModuleFileSystem::GetExtension(const char *file) const
+//{
+//	std::string filename(file);
+//	std::size_t found = filename.find_last_of(".");
+//	if (std::string::npos != found)
+//	{
+//		filename.erase(0, found + 1);
+//	}
+//	return filename.c_str();
+//}
 
 
--- Source/ModuleFileSystem.h
+++ Source/ModuleFileSystem.h
@@ -9,13 +9,15 @@ public:
 	ModuleFileSystem();
 	~ModuleFileSystem();
 
+	bool Init();
+
 	unsigned int Load(const char* path, const char* file, char** buffer) const;
 	unsigned int Save(const char* file, const char* buffer, unsigned size) const;
 	bool Remove(const char* file) const;
 	bool Exists(const char* file) const;
+	unsigned Size(const char* file) const;
 	bool MakeDirectory(const char* directory);
 	bool IsDirectory(const char* file) const;
-	const char * GetExtension(const char * file) const;
 	bool Copy(const char* source, const char* destination);
 };
 
