a9ccabc7dc54329e4bc9fcb57889ee7ad3f348b7
Solidajenjo <solidajenjo@gmail.com>
2019-03-16 16:37:25 +0100

    Created game loop

---
+++ Scripts/GameLoop/GameLoop.h
@@ -0,0 +1,32 @@
+#ifndef  __GameLoop_h__
+#define  __GameLoop_h__
+
+#include "BaseScript.h"
+
+#ifdef GameLoop_EXPORTS
+#define GameLoop_API __declspec(dllexport)
+#else
+#define GameLoop_API __declspec(dllimport)
+#endif
+
+class GameLoop_API GameLoop : public Script
+{
+	enum class GameState
+	{		
+		MENU,
+		INTRO,
+		PLAYING,
+		PAUSED,
+		CREDITS,
+		QUIT
+	};
+
+	void Start() override;
+	void Update() override;
+
+	GameState gameState = GameState::MENU;
+};
+
+extern "C" GameLoop_API GameLoop* CreateScript();
+
+#endif __GameLoop_h__
