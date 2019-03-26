586c5eb6f62b83ebb531863ad998a8bf10f1dad0
lluissr <lluissr@gmail.com>
2019-03-26 19:49:05 +0100

    Access menu controls and manage sound in play

--- Scripts/GameLoop/GameLoop.h
+++ Scripts/GameLoop/GameLoop.h
@@ -25,6 +25,7 @@ class GameLoop_API GameLoop : public Script
 		PAUSED,
 		OPTIONS,
 		CREDITS,
+		CONTROLS,
 		QUIT
 	};
 
@@ -37,10 +38,12 @@ class GameLoop_API GameLoop : public Script
 	void ManagePaused();
 	void ManageOptions();
 	void ManageCredits();
+	void ManageControls();
 	void ManageQuit();
 
 	void EnableMenuButtons(bool enable);
 	void VolumeManagement();
+	void SoundManagement();
 
 	void ChangeGameState(GameState newState); //Set initial conditions for each state here if required
 
@@ -49,22 +52,30 @@ class GameLoop_API GameLoop : public Script
 	//UI Values
 	int volume = 10;
 	int minVolume = 0;
-	int maxVolume = 20;
+	int maxVolume = 10;
+	int sound = 6;
+	int minSound = 0;
+	int maxSound = 10;
 
 	//UI
 
 	//Buttons
 	std::vector<Component*> menuButtons;
 	ComponentButton* optionButton = nullptr;
+	ComponentButton* controlsButton = nullptr;
 	ComponentButton* backOptionButton = nullptr;
+	ComponentButton* backControlsButton = nullptr;
 	std::vector<Component*> volumeButtons;
+	std::vector<Component*> soundButtons;
 
 	//GO
 	GameObject* menu = nullptr;
 	GameObject* options = nullptr;
+	GameObject* controls = nullptr;
 
 	//TEXT
 	ComponentText* volumeText = nullptr;
+	ComponentText* soundText = nullptr;
 
 	//Script
 	//Script* intro = nullptr;
