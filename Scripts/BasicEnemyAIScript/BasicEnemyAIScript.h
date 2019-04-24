6793edf65fa8da86089434fb3dce33e5362a7992
Rubén Crispín De la Cruz <ruben.crispin@hotmail.com>
2019-04-24 19:32:34 +0200

    Added states and Stand-up logic

--- Scripts/BasicEnemyAIScript/BasicEnemyAIScript.h
+++ Scripts/BasicEnemyAIScript/BasicEnemyAIScript.h
@@ -9,10 +9,32 @@
 #define BasicEnemyAIScript_API __declspec(dllimport)
 #endif
 
+enum class EnemyState
+{
+	STANDUP,
+	CHASE,
+	ATTACK,
+	COOLDOWN,
+	DEAD
+};
+
 class BasicEnemyAIScript_API BasicEnemyAIScript : public Script
 {
+public:
 	void Start() override;
+	void Update() override;
 
+	void Expose(ImGuiContext* context) override;
+
+public:
+	EnemyState enemyState = EnemyState::STANDUP;
+
+	// Stand-Up variables
+	float standupSpeed = 1.0f;
+	float yTranslation = 20.0f;
+
+private:
+	float auxTranslation = 0.0f;
 };
 
 #endif __BasicEnemyAIScript_h__
