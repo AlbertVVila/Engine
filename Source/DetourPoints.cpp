ea16afdf6d50c03b48f9ca53115ad3b96abdfe92
Gabroide <ing.geol@gmail.com>
2019-04-24 15:32:39 +0200

    New Class to intput the start and end points

---
+++ Source/DetourPoints.cpp
@@ -0,0 +1,25 @@
+#include "DetourPoints.h"
+
+DetourPoints::DetourPoints()
+{
+	WOWPOS start = { -8949.95f, -132.493f, 83.5312f };
+	WOWPOS end = { -9046.507f, -45.71962f, 88.33186f };
+
+	WOWPOS *path = new WOWPOS[MAX_PATH];
+
+	int size = MAX_PATH;
+	int pathSize = navQuery->FindStraightPath(start, end, path, size);
+
+
+	LOG("FindStraightPath: returned ", pathSize, " points.");
+
+	for (int i = 0; i < pathSize; i++)
+	{
+		LOG("   ... X=", path[i].x, ", Y=", path[i].y, ", Z=", path[i].z);
+	}
+}
+
+
+DetourPoints::~DetourPoints()
+{
+}
