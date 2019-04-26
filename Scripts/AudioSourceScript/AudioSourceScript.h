8f3afd30fbd2617f5a259cea9ffc0ad9648c3e32
Marc <marc.palomar.soler@gmail.com>
2019-04-26 17:11:59 +0200

    Audio Script created

---
+++ Scripts/AudioSourceScript/AudioSourceScript.h
@@ -0,0 +1,23 @@
+#ifndef  __AudioSourceScript_h__
+#define  __AudioSourceScript_h__
+
+#include "BaseScript.h"
+
+#ifdef AudioSourceScript_EXPORTS
+#define AudioSourceScript_API __declspec(dllexport)
+#else
+#define AudioSourceScript_API __declspec(dllimport)
+#endif
+
+class ComponentAudioSource;
+
+class AudioSourceScript_API AudioSourceScript : public Script
+{
+	void Start() override;
+	void Update() override;
+
+private:
+	ComponentAudioSource* audioSource = nullptr;
+};
+
+#endif __AudioSourceScript_h__
