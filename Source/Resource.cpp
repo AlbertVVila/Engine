b056d67a491f4e616a28827a3718a6526e28090b
cromerop <lituslitus16@gmail.com>
2019-05-14 00:42:38 +0200

    Meshes maintain UID as name

--- Source/Resource.cpp
+++ Source/Resource.cpp
@@ -77,12 +77,11 @@ void Resource::Rename(const char* newName)
 	std::string fileInLibrary = App->fsystem->GetFile(exportedFile);
 	std::string exportedExtension = App->fsystem->GetExtension(exportedFile);
 
-	if (type != TYPE::MODEL)
+	if (type != TYPE::MODEL && type != TYPE::MESH)
 	{
 		// Rename of file in Library
 		App->fsystem->Rename(ruteToExportedFile.c_str(), fileInLibrary.c_str(), newName);
 	}
-
 	// Update exportedFile variable
 	exportedFile = (ruteToExportedFile + newName + exportedExtension).c_str();
 
