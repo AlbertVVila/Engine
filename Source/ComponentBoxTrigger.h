8f1c398209c61882750944942e4ba97cda5b3a96
AlbertVVila <albert.val.vila@est.fib.upc.edu>
2019-05-23 11:46:47 +0200

    Corrected coding style

--- Source/ComponentBoxTrigger.h
+++ Source/ComponentBoxTrigger.h
@@ -3,10 +3,12 @@
 
 #include "Component.h"
 #include <unordered_map>
-#include "Geometry/OBB.h"
 #include "Math/float3.h"
+#include "Geometry/OBB.h"
 
-enum class Overlap_State
+class OBB;
+
+enum class OverlapState
 {
 	Enter = 0,
 	Idle,
@@ -32,15 +34,15 @@ public:
 	void SetIsPlayer(bool isPlayer);
 	inline bool GetIsPlayer() const { return isPlayer; }
 
-	inline const math::OBB* GetOBB() const { return box_trigger; }
+	inline const math::OBB* GetOBB() const { return boxTrigger; }
 
 	inline math::float3 GetBoxCenter() const { return position; }
-	inline void SetBoxCenter(math::float3 center) { position = center; box_trigger->pos = center; }
-	inline void SetBoxPosition(float x, float y, float z) { position = math::float3(x, y, z); box_trigger->pos = position; }
+	inline void SetBoxCenter(math::float3 center) { position = center; boxTrigger->pos = center; }
+	inline void SetBoxPosition(float x, float y, float z) { position = math::float3(x, y, z); boxTrigger->pos = position; }
 
 	inline math::float3 GetBoxSize() const { return size; }
-	inline void SetBoxSize(math::float3 sizes) { size = sizes;  box_trigger->r = sizes; }
-	inline void SetBoxSize(float width, float height, float depth) { size = math::float3(width, height, depth); box_trigger->r = size; }
+	inline void SetBoxSize(math::float3 sizes) { size = sizes;  boxTrigger->r = sizes; }
+	inline void SetBoxSize(float width, float height, float depth) { size = math::float3(width, height, depth); boxTrigger->r = size; }
 
 	void AddOverlap(const ComponentBoxTrigger* other);
 	void RemoveOverlap(const ComponentBoxTrigger* other);
@@ -49,11 +51,11 @@ public:
 	void Load(JSON_value* value);
 
 private:
-	void PropagateState(GameObject* other, Overlap_State state);
+	void PropagateState(GameObject* other, OverlapState state);
 
 private:
-	std::unordered_map<const ComponentBoxTrigger*,Overlap_State> overlap_list;
-	math::OBB* box_trigger = nullptr;
+	std::unordered_map<const ComponentBoxTrigger*,OverlapState> overlapList;
+	math::OBB* boxTrigger = nullptr;
 
 	math::float3 position = math::float3::zero;
 	math::float3 size     = math::float3::one;
