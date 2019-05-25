7f14b22faec5de49482c752d9c7b2717076d2360
Ruben <solidajenjo@gmail.com>
2019-05-25 17:59:22 +0200

    Shader albedo alpha

--- Game/Shaders/FragmentShaders/PBR.fs
+++ Game/Shaders/FragmentShaders/PBR.fs
@@ -256,7 +256,7 @@ void main()
 	
 	float brightness = dot(Fragcolor.rgb, vec3(0.2126, 0.7152, 0.0722));
     if(brightness > 1.0)
-        brightColor = vec4(Fragcolor.rgb, 1.0);
+        brightColor = vec4(Fragcolor.rgb, albedo.a);
     else
         brightColor = vec4(0.0, 0.0, 0.0, 1.0);
 
