75f0aa12670318ac34d8506dd8cd6b04dbf89033
Rubén Crispín De la Cruz <ruben.crispin@hotmail.com>
2019-05-30 15:01:27 +0200

    Fixed Hidden enemy hit box

--- Scripts/HiddenEnemyAIScript/EnemyStateAttack.cpp
+++ Scripts/HiddenEnemyAIScript/EnemyStateAttack.cpp
@@ -32,22 +32,22 @@ void EnemyStateAttack::Update()
 		if (hitboxCreated)
 		{
 			// Disable hitbox
-			enemy->boxTrigger->Enable(false);
+			enemy->enemyController->attackBoxTrigger->Enable(false);
 			hitboxCreated = false;
 		}
 		enemy->currentState = (EnemyState*)enemy->chase;
 	}
 	else
 	{
-		assert(enemy->boxTrigger != nullptr);
+		assert(enemy->enemyController->attackBoxTrigger != nullptr);
 		if (!hitboxCreated)
 		{
 			// Attack
 			//Create the hitbox
-			enemy->boxTrigger->Enable(true);
-			enemy->boxTrigger->SetBoxSize(boxSize);
+			enemy->enemyController->attackBoxTrigger->Enable(true);
+			enemy->enemyController->attackBoxTrigger->SetBoxSize(boxSize);
 			boxPosition = enemy->gameobject->transform->up * 100.f;
-			enemy->boxTrigger->SetBoxPosition(boxPosition.x, boxPosition.y, boxPosition.z + 100.f);
+			enemy->enemyController->attackBoxTrigger->SetBoxPosition(boxPosition.x, boxPosition.y, boxPosition.z + 100.f);
 			hitboxCreated = true;
 
 			//playerScript->Damage(attackDamage);
@@ -57,7 +57,7 @@ void EnemyStateAttack::Update()
 		{
 			// End attack
 			// Disable hitbox
-			enemy->boxTrigger->Enable(false);
+			enemy->enemyController->attackBoxTrigger->Enable(false);
 			hitboxCreated = false;
 
 			// Enter cooldown state
