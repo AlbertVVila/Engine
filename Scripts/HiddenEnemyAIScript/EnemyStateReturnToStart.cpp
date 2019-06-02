afeddb043a45aa6c540dce47521d4d731cd8db8e
Rubén Crispín De la Cruz <ruben.crispin@hotmail.com>
2019-06-02 21:58:29 +0200

    Changed offset value for a define

--- Scripts/HiddenEnemyAIScript/EnemyStateReturnToStart.cpp
+++ Scripts/HiddenEnemyAIScript/EnemyStateReturnToStart.cpp
@@ -3,6 +3,8 @@
 #include "HiddenEnemyAIScript.h"
 #include "EnemyControllerScript.h"
 
+#define START_POS_OFFSET  20.5f
+
 EnemyStateReturnToStart::EnemyStateReturnToStart(HiddenEnemyAIScript* AIScript)
 {
 	enemy = AIScript;
@@ -34,7 +36,7 @@ void EnemyStateReturnToStart::Update()
 		//Check distance to start position
 		// TODO: Change to check that there is not a nearest point on the nav mesh to this
 		float distanceToStartPosition = enemy->enemyController->GetDistanceTo2D(enemy->startPosition);
-		if (distanceToStartPosition < 20.5f)
+		if (distanceToStartPosition < START_POS_OFFSET)
 			enemy->currentState = (EnemyState*)enemy->hide;
 	}
 }
