d8b0c3b3ce6b1ff98c6a3e4c7bb01069b54be4f3
SasiKoka <borja.sasieta@gmail.com>
2019-06-10 14:59:57 +0200

    Repared the F to center
    Removed the innecessari if

--- Source/ModuleCamera.cpp
+++ Source/ModuleCamera.cpp
@@ -67,15 +67,12 @@ update_status ModuleCamera::Update(float dt)
 {
 	BROFILER_CATEGORY("Camera Update", Profiler::Color::Red)
 	
-	if (App->renderer->IsSceneViewFocused() || App->editor->hierarchy->IsHovered())
+	if ((App->renderer->IsSceneViewFocused() || App->editor->hierarchy->IsHovered()))
 	{
-		if(App->renderer->IsSceneHovered())
-		{
 		InputMove(dt);
 		InputRotate(dt);
 		InputCenter();
 		InputOrbit(dt);
-		}
 	}
 	if (App->renderer->IsSceneHovered())
 	{
