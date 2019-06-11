ea77548da63ff6b71445517e2e56d94a7e9a4b7a
Lluis <lluissr@gmail.com>
2019-06-12 00:16:00 +0200

    Added Expose and Serialize methods

--- Scripts/ExperienceController/ExperienceController.h
+++ Scripts/ExperienceController/ExperienceController.h
@@ -13,11 +13,16 @@ class Text;
 class ComponentImage;
 class SkillTreeController;
 class GameObject;
+class JSON_value;
 
 class ExperienceController_API ExperienceController : public Script
 {
 	void Start() override;
 	void Update() override;
+
+	void Expose(ImGuiContext* context) override;
+	void Serialize(JSON_value* json) const override;
+	void DeSerialize(JSON_value* json) override;
 
 public:
 	void AddXP(int xp);
@@ -36,7 +41,7 @@ private:
 	int currentLevel = 1;
 	int maxXPLevel = 100;
 	int maxLevel = 10;
-	int levelsExp[10] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+	int levelsExp[23] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300 };
 	int totalXPAcumulated = 0;
 	bool updateXP = false;
 	bool levelUP = false;
