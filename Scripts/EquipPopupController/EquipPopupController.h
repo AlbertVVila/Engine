230933b76131afc86f03d3fcf125b2d3df035fdd
lluissr <lluissr@gmail.com>
2019-06-17 19:58:29 +0200

    Now can open and close Popup

--- Scripts/EquipPopupController/EquipPopupController.h
+++ Scripts/EquipPopupController/EquipPopupController.h
@@ -2,6 +2,7 @@
 #define  __EquipPopupController_h__
 
 #include "BaseScript.h"
+#include <vector>
 
 #ifdef EquipPopupController_EXPORTS
 #define EquipPopupController_API __declspec(dllexport)
@@ -9,8 +10,28 @@
 #define EquipPopupController_API __declspec(dllimport)
 #endif
 
+class GameObject;
+class Text;
+class ComponentImage;
+class Button;
+
 class EquipPopupController_API EquipPopupController : public Script
 {
+
+	void Start() override;
+	void Update() override;
+
+	void FillSlots();
+
+private:
+	GameObject* popupGO = nullptr;
+	ComponentImage* background = nullptr;
+	Text* title = nullptr;
+	Button* items = nullptr;
+	Button* skills = nullptr;
+	Button* unequip = nullptr;
+	std::vector<Button*> hudButtons;
+	Button* activeButton = nullptr;
 };
 
 #endif __EquipPopupController_h__
