931654d6a49bf273acbc611fbfe58fee7d3da2e7
Lluis <lluissr@gmail.com>
2019-06-18 21:19:59 +0200

    Working in popup script

--- Scripts/EquipPopupController/EquipPopupController.h
+++ Scripts/EquipPopupController/EquipPopupController.h
@@ -14,6 +14,10 @@ class GameObject;
 class Text;
 class ComponentImage;
 class Button;
+class Item;
+class Skill;
+class SkillTreeController;
+class InventoryScript;
 
 class EquipPopupController_API EquipPopupController : public Script
 {
@@ -21,17 +25,35 @@ class EquipPopupController_API EquipPopupController : public Script
 	void Start() override;
 	void Update() override;
 
-	void FillSlots();
+	void Assign(int i);
+
+
 
 private:
+
+	void ChangePopUpSlots();
+	void FillLists();
+	void FillSkillSlots();
+	void FillItemSlots();
+
+	InventoryScript* inventory = nullptr;
+	SkillTreeController* skillTree = nullptr;
+
 	GameObject* popupGO = nullptr;
 	ComponentImage* background = nullptr;
 	Text* title = nullptr;
 	Button* items = nullptr;
 	Button* skills = nullptr;
 	Button* unequip = nullptr;
+	std::vector<GameObject*> slots;
+
 	std::vector<Button*> hudButtons;
 	Button* activeButton = nullptr;
+
+	std::vector<Item> itemsList;
+	std::vector<Skill> skillsList;
+
+	bool skillsShowing = true;
 };
 
 #endif __EquipPopupController_h__
