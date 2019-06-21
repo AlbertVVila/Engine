f3f30eaa7784bb73c9a88ea380216b5a077d41f5
Rubén Crispín De la Cruz <ruben.crispin@hotmail.com>
2019-06-21 16:31:09 +0200

    Player can move while using CircularAttackSkill

--- Scripts/PlayerMovement/CircularAttackSkill.cpp
+++ Scripts/PlayerMovement/CircularAttackSkill.cpp
@@ -4,12 +4,16 @@
 
 
 #include "ModuleTime.h"
+#include "ModuleInput.h"
+#include "ModuleNavigation.h"
 
 #include "GameObject.h"
 #include "ComponentTransform.h"
 #include "ComponentBoxTrigger.h"
 
 #include "PlayerState.h"
+
+#include "Math/float2.h"
 
 CircularAttackSkill::CircularAttackSkill(PlayerMovement* PM, const char* trigger, ComponentBoxTrigger* attackBox) :
 	MeleeSkill(PM, trigger, attackBox)
@@ -41,6 +45,8 @@ void CircularAttackSkill::Update()
 		attackBoxTrigger->SetBoxSize(boxSize);
 		atatckStarted = true;
 	}
+
+	CheckInput();
 }
 
 void CircularAttackSkill::UseSkill()
@@ -77,5 +83,68 @@ void CircularAttackSkill::Reset()
 	spinTimer = 0.0f;
 	atatckStarted = false;
 	MeleeSkill::Reset();
+}
+
+void CircularAttackSkill::CheckInput()
+{
+	// Once the attack is finished
+	if (timer > player->currentState->duration)
+	{
+		if (player->IsUsingSkill())
+		{
+			player->currentState = (PlayerState*)player->attack;
+		}
+		else if (player->IsMoving())
+		{
+			player->currentState = (PlayerState*)player->walk;
+		}
+		else
+		{
+			Reset();
+		}
+	}
+	else
+	{
+		// Move while using the attack
+		if (player->IsMoving())
+		{
+		math:float2 mouse((float*)&player->App->input->GetMousePosition());
+			if (player->App->input->GetMouseButtonDown(3) == KEY_DOWN
+				|| player->App->input->GetMouseButtonDown(3) == KEY_REPEAT)
+			{
+				moveTimer = 0.0f;
+				math::float3 intPos(0.f, 0.f, 0.f);
+				if (player->App->navigation->NavigateTowardsCursor(player->gameobject->transform->position, path,
+					math::float3(player->OutOfMeshCorrectionXZ, player->OutOfMeshCorrectionY, player->OutOfMeshCorrectionXZ),
+					intPos, player->maxWalkingDistance))
+				{
+					//case the player clicks outside of the floor mesh but we want to get close to the floors edge
+					pathIndex = 0;
+				}
+				else
+				{
+					return;
+				}
+			}
+			else if (player->App->input->GetMouseButtonDown(3) == KEY_REPEAT)
+			{
+				moveTimer += player->App->time->gameDeltaTime;
+			}
+			if (path.size() > 0)
+			{
+				math::float3 currentPosition = player->gameobject->transform->GetPosition();
+				while (pathIndex < path.size() && currentPosition.DistanceSq(path[pathIndex]) < MINIMUM_PATH_DISTANCE)
+				{
+					pathIndex++;
+				}
+				if (pathIndex < path.size())
+				{
+					player->gameobject->transform->LookAt(path[pathIndex]);
+					math::float3 direction = (path[pathIndex] - currentPosition).Normalized();
+					player->gameobject->transform->SetPosition(currentPosition + player->walkingSpeed * direction * player->App->time->gameDeltaTime);
+				}
+			}
+		}
+	}
 }
 
--- Scripts/PlayerMovement/CircularAttackSkill.h
+++ Scripts/PlayerMovement/CircularAttackSkill.h
@@ -2,6 +2,10 @@
 #define  __CircularAttackSkill_h__
 
 #include "MeleeSkill.h"
+
+#include <vector>
+#include "Math/float3.h"
+
 class CircularAttackSkill :
 	public MeleeSkill
 {
@@ -16,11 +20,19 @@ protected:
 	void UseSkill() override;
 	void Reset() override;
 
+	void CheckInput() override;
+
 private:
 	float fullSpinTime = 0.4f;	// Time it takes to the player to draw a full circle spinning
 	float spinTimer = 0.0f;		// Timer to check time performing a skill
 
 	bool atatckStarted = false;
+
+	// Move variables
+	float duration = 1.5f;
+	std::vector<float3>path;
+	unsigned pathIndex = 0;
+	float moveTimer = 0.0f;
 };
 
 #endif __CircularAttackSkill_h__
