4dba3b7e1db52cb1fa5742cca13ab6f053169f05
sondy99@gmail.com <sondy99@gmail.com>
2019-07-15 22:02:59 +0200

    Learning how manage cursor change loaded from file

--- Source/PanelTime.cpp
+++ Source/PanelTime.cpp
@@ -1,3 +1,6 @@
+#define OEMRESOURCE
+#include <windows.h>
+
 #include "Application.h"
 
 #include "ModuleWindow.h"
@@ -46,11 +49,21 @@ void PanelTime::Draw()
 			App->time->StartGameClock();
 			App->scene->SaveTemporaryScene();
 			App->scene->root->OnPlay();
+
+			HCURSOR handCursor = LoadCursorFromFile("C:\\Windows\\Cursors\\aero_link_l.cur");
+			BOOL ret = SetSystemCursor(handCursor, OCR_NORMAL);
+			assert(ret);
+			DestroyCursor(handCursor);
 		}
 		else 
 		{
 			App->time->StopGameClock();
 			App->scene->LoadTemporaryScene();
+
+			HCURSOR arrowCursor = LoadCursorFromFile("C:\\Windows\\Cursors\\aero_arrow_l.cur");
+			BOOL ret = SetSystemCursor(arrowCursor, OCR_NORMAL);
+			assert(ret); 
+			DestroyCursor(arrowCursor);
 		}
 	}
 
