d045a48bf313e4c0d4869e11727dfb2c702384f2
Aniol <aniol92@hotmail.com>
2019-09-06 20:42:59 +0200

    Buried prefab now fully working

--- Scripts/BuriedEnemyAIScript/EnemyStateDeath.cpp
+++ Scripts/BuriedEnemyAIScript/EnemyStateDeath.cpp
@@ -1,14 +1,105 @@
 #include "EnemyStateDeath.h"
 
+#include "GameObject.h"
+
+#include "Application.h"
+#include "ModuleScene.h"
+#include "ModuleTime.h"
+
+#include "ComponentRenderer.h"
+#include "ComponentTransform.h"
+#include "BuriedEnemyAIScript.h"
+
+#include "Math/float3.h"
+#include "Algorithm/Random/LCG.h"
+
 
 
 EnemyStateDeath::EnemyStateDeath(BuriedEnemyAIScript* AIScript)
 {
 	enemy = AIScript;
 	trigger = "Death";
+
+	renderer = (ComponentRenderer*)enemy->gameobject->GetComponentInChildren(ComponentType::Renderer);
+
+	bonesParent = enemy->App->scene->FindGameObjectByName("DeathBones", enemy->gameobject);
+	bonesDeathFX = enemy->App->scene->FindGameObjectByName("DeathFX", enemy->gameobject);
+	LCG rand;
+
+	if (bonesParent != nullptr)
+	{
+		for (GameObject* bone : bonesParent->children)
+		{
+			deathBones.emplace_back(bone);
+			boneInfo.emplace_back(std::make_pair(math::float3(rand.Float() * 2 - 1, rand.Float(), rand.Float() * 2 - 1),
+				math::float3(rand.Float() * 360, rand.Float() * 360, rand.Float() * 360)));
+		}
+	}
 }
 
 
 EnemyStateDeath::~EnemyStateDeath()
 {
+}
+
+void EnemyStateDeath::DeathAnimation()
+{
+	for (unsigned i = 0; i < deathBones.size(); ++i)
+	{
+		math::float3 currentPosition = deathBones[i]->transform->GetPosition();
+		deathBones[i]->transform->SetPosition(currentPosition + boneInfo[i].first * bonesSpeed * enemy->App->time->gameDeltaTime);
+
+		//math::Quat currentRotation = deathBones[i]->transform->GetRotation();
+		//deathBones[i]->transform->SetRotation(currentRotation.Mul(
+		//	Quat::FromEulerXYZ(boneInfo[i].second.x* rotationSpeed * enemy->App->time->gameDeltaTime, 
+		//		boneInfo[i].second.y * rotationSpeed * enemy->App->time->gameDeltaTime,
+		//		boneInfo[i].second.z * rotationSpeed * enemy->App->time->gameDeltaTime)));
+
+		deathBones[i]->GetComponent<ComponentRenderer>()->dissolveAmount += enemy->App->time->gameDeltaTime;
+	}
+}
+
+void EnemyStateDeath::Update()
+{
+	float waitedTime = (timer - auxTimer);
+
+	if (waitedTime > deathDuration * 0.3f)
+	{
+		if (bonesDeathFX != nullptr && bonesDeathFX->isActive())
+		{
+			bonesDeathFX->SetActive(false);
+		}
+	}
+
+	if (waitedTime > deathDuration)
+	{
+		enemy->gameobject->SetActive(false);
+		if (bonesParent != nullptr)
+		{
+			bonesParent->SetActive(false);
+		}
+	}
+	else if (waitedTime > deathDuration*0.1f)
+	{
+		if (bonesDeathFX != nullptr && bonesDeathFX->children.front()->isActive())
+		{
+			bonesDeathFX->children.front()->SetActive(false);
+		}
+		renderer->dissolveAmount = waitedTime / deathDuration;
+		DeathAnimation();
+	}
+}
+
+void EnemyStateDeath::Enter()
+{
+	GameObject* meshScene = enemy->gameobject->children.front();
+	meshScene->children.front()->SetActive(false); //disactive bones with its trails
+	auxTimer = 0.0f;
+	if (bonesParent != nullptr)
+	{
+		bonesParent->SetActive(true);
+		bonesDeathFX->SetActive(true);
+	}
+	renderer->highlighted = false;
+
 }
--- Scripts/BuriedEnemyAIScript/EnemyStateDeath.h
+++ Scripts/BuriedEnemyAIScript/EnemyStateDeath.h
@@ -2,12 +2,34 @@
 #define __ENEMYSTATEDEATH_H_
 
 #include "EnemyState.h"
+
+class GameObject;
+class ComponentRenderer;
+
 class EnemyStateDeath :
 	public EnemyState
 {
 public:
 	EnemyStateDeath(BuriedEnemyAIScript* AIScript);
 	~EnemyStateDeath();
+
+	void Enter() override;
+	void Update() override;
+
+private:
+	void DeathAnimation();
+
+	GameObject* bonesParent = nullptr;
+	GameObject* bonesDeathFX = nullptr;
+
+	std::vector<GameObject*> deathBones;
+	std::vector<std::pair<math::float3, math::float3>> boneInfo;
+
+	ComponentRenderer* renderer = nullptr;
+
+	float bonesSpeed = 2000.0f;
+	float rotationSpeed = 0.05f;
+	float deathDuration = 0.5f;
 };
 
 #endif // __ENEMYSTATEDEATH_H_
