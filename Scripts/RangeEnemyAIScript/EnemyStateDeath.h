87dea86b0a3a1c81ea928ad428e6a12b3eeb0533
Aniol <aniol92@hotmail.com>
2019-09-06 21:11:06 +0200

    Added functional prefab for bandolero

---
+++ Scripts/RangeEnemyAIScript/EnemyStateDeath.h
@@ -0,0 +1,35 @@
+#ifndef __ENEMYSTATEDEATH_H_
+#define __ENEMYSTATEDEATH_H_
+
+#include "EnemyState.h"
+
+class GameObject;
+class ComponentRenderer;
+
+class EnemyStateDeath :
+	public EnemyState
+{
+public:
+	EnemyStateDeath(RangeEnemyAIScript* AIScript);
+	~EnemyStateDeath();
+
+	void Enter() override;
+	void Update() override;
+
+private:
+	void DeathAnimation();
+
+	GameObject* bonesParent = nullptr;
+	GameObject* bonesDeathFX = nullptr;
+
+	std::vector<GameObject*> deathBones;
+	std::vector<std::pair<math::float3, math::float3>> boneInfo;
+
+	ComponentRenderer* renderer = nullptr;
+
+	float bonesSpeed = 2000.0f;
+	float rotationSpeed = 0.05f;
+	float deathDuration = 0.5f;
+};
+
+#endif // __ENEMYSTATEDEATH_H_
