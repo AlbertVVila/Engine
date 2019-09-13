88db93cf03210cbf4110b3237039f7dbc4d8ff64
Aniol <aniol92@hotmail.com>
2019-09-13 23:34:42 +0200

    Added AOE script, not fully functional

---
+++ Scripts/AOEBossScript/AOEBossScript.h
@@ -0,0 +1,43 @@
+#ifndef  __AOEBossScript_h__
+#define  __AOEBossScript_h__
+
+#include "BaseScript.h"
+#include <vector>
+#include "Math/float3.h"
+
+#ifdef AOEBossScript_EXPORTS
+#define AOEBossScript_API __declspec(dllexport)
+#else
+#define AOEBossScript_API __declspec(dllimport)
+#endif
+
+class AOEBossScript_API AOEBossScript : public Script
+{
+public:
+	void Awake() override;
+	void Start() override;
+	void Update() override;
+
+	void Expose(ImGuiContext* context) override;
+
+	void Serialize(JSON_value* json) const override;
+	void DeSerialize(JSON_value* json) override;
+
+	inline virtual AOEBossScript* Clone() const
+	{
+		return new AOEBossScript(*this);
+	}
+private:
+
+	GameObject* prepParticlesGO = nullptr;
+	GameObject* beamParticlesGO = nullptr;
+	GameObject* boxTrigger = nullptr;
+
+	float duration = 3.0f;
+	float timerFade = 2.0f;
+	float timer = 0.0f;
+};
+
+extern "C" AOEBossScript_API Script* CreateScript();
+
+#endif __AOEBossScript_h__
