26e5da083d154346495f6ab318a2ab6cb8920413
Rubén Crispín De la Cruz <ruben.crispin@hotmail.com>
2019-09-28 22:51:37 +0200

    Created scale script base

---
+++ Scripts/ScaleOverTimeScript/ScaleOverTimeScript.h
@@ -0,0 +1,30 @@
+#ifndef  __ScaleOverTimeScript_h__
+#define  __ScaleOverTimeScript_h__
+
+#include "BaseScript.h"
+
+#include "MathGeoLib/Math/float3.h"
+
+#ifdef ScaleOverTimeScript_EXPORTS
+#define ScaleOverTimeScript_API __declspec(dllexport)
+#else
+#define ScaleOverTimeScript_API __declspec(dllimport)
+#endif
+
+class ScaleOverTimeScript_API ScaleOverTimeScript : public Script
+{
+	void Start() override;
+	void Update() override;
+
+	void Expose(ImGuiContext* context);
+
+	void Serialize(JSON_value* json) const;
+	void DeSerialize(JSON_value* json);
+
+public:
+	float speed = 1.0f;
+	math::float3 finalScale = math::float3::one;
+	float timer = 0.0f;
+};
+
+#endif __ScaleOverTimeScript_h__
